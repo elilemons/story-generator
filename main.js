7bae8591929e5b0cf490fa352f706dbead5a0c1e
Lisa Lemons - Grand Circus <LisaLemonsGrandCircus@Katies-MacBook-Air.local>
2019-03-12 20:13:18 -0400

    i got carried away

--- main.js
+++ main.js
@@ -2,8 +2,11 @@
 $(() => {
 
   class Character {
-    constructor(name, attachments, type, notes) {
-
+    constructor(name, image, type, notes) {
+      this.name = name;
+      this.image = image;
+      this.type = type;
+      this.notes = notes;
     }
   }
 
@@ -14,21 +17,73 @@ $(() => {
       this.API = `https://api.airtable.com/v0/${this.BASE_ID}`;
     }
 
-    get(tableName, callback) {
-      $.ajax({
+    get(tableName) {
+      return $.ajax({
         url: `${this.API}/${tableName}`,
         type: 'GET',
         headers: {
           Authorization: `Bearer ${this.API_KEY}`
         },
-      }).then((data) => {
-        callback(data);
+      })
+      .fail((error) => {
+        console.error(`There was an error: ${error}`);
+      })
+      .done((data) => {
+        localStorage.setItem(tableName, JSON.stringify(data.records));
       });
     }
   }
 
+  function setupCharacters(characters) {
+    for (let character of characters) {
+      let char = new Character(
+          character.fields.Name,
+          character.fields.Image[0],
+          character.fields.Type,
+      );
+
+      $('#list-of-characters').append(`<li id="${character.id}-list-item" class="list-group-item list-group-item-action">${char.name}</li>`);
+      $(`#${character.id}-list-item`).on('click',() => {
+        displayCharacter(char);
+        $('.list-group-item').removeClass('active');
+        $(`#${character.id}-list-item`).addClass('active');
+      });
+
+      if (char.name.indexOf('Steven') > -1) {
+        $(`#${character.id}-list-item`).click();
+      }
+    }
+  }
+
+  function displayCharacter(character) {
+    $('#character-image').attr('src', character.image.url);
+    $('#character-name').text(character.name);
+    $('#character-notes').text(character.notes);
+    let typeHTML,
+        typeCSSClass = '';
+    for (let type of character.type) {
+      if (type === 'Gem') {
+        typeCSSClass += 'type-gem';
+      }
+
+      if (type === 'Human') {
+        typeCSSClass += 'type-human'
+      }
+
+      if (type === 'Mystical Creature') {
+        typeCSSClass += 'type-mystical-creature';
+      }
+      typeHTML = $(`<span class="badge ${typeCSSClass}">${type}</span>`);
+    }
+
+    $('#character-types').html(typeHTML);
+    
+  }
+
   let base = new Airtable;
-  let characters = base.get('Characters', (data) => {
-    console.log(data);
-  });
+  if (localStorage.getItem('Characters')) {
+    setupCharacters(JSON.parse(localStorage.getItem('Characters')));
+  } else {
+    base.get('Characters').then((data) => setupCharacters(data.records));
+  }
 });
